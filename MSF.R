abd3cd0d3d03182f1021337a77c1bcbca5f56292
Praveen Kumar Neelappa <pnkumar@outlook.com>
2018-03-18 15:24:13 -0400

    first update

---
+++ MSF.R
@@ -0,0 +1,32 @@
+require(caret) 
+require(e1071) 
+tweetdat <- read.csv('MSF_Complete_No_Missing_Updated_10p.csv', as.is = T);
+attach(eddat)
+
+head(tweetdat)
+
+tweetdat$class <- as.factor(tweetdat$class)
+
+dm <- cbind(tweetdat["class"], scale(tweetdat[,c(2,4,5,6,7,8,9,11,12,13,15,16,18,19,20,21)]), tweetdat[c(1,3,10,14,17)])
+
+dim(dm)
+
+start_time <- Sys.time()
+Accuracies <- c(0.00)
+
+sink('sinktest.txt')
+for (i in seq(100))
+{
+  
+  inTrain <- createDataPartition(dm$class, p = .80, list = FALSE)
+  dm.navg <- avNNet(dm[c(2:22)], dm$class, subset = inTrain, size = 2, rang= 0.5, decay = 5e-6, maxit = 200, repeats = 50)
+  
+  Accuracies[i] <- confusionMatrix(dm$class[-inTrain], predict(dm.navg, dm[-inTrain,], type = "class"))$overall["Accuracy"]
+}
+
+sink()
+summary(Accuracies)
+
+
+acclen = length(na.omit(Accuracies))
+plot(density(Accuracies), main = paste('Average Neural Network Model accuracies:',acclen, 'runs') )
